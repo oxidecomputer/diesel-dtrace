d33ac33940469ea1336ddf77217dcf7ec9d164c7
bnaecker <ben@oxide.computer>
2023-01-30 09:58:38 -0800

    Add better detection of needed features (#15)

--- src/lib.rs
+++ src/lib.rs
@@ -25,7 +25,7 @@
 //! on a connection being established or query executing (e.g., tracing all system calls while a
 //! query is running).
 
-// Copyright 2021 Oxide Computer Company
+// Copyright 2023 Oxide Computer Company
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -39,7 +39,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#![cfg_attr(target_os = "macos", feature(asm_sym))]
+#![cfg_attr(usdt_need_asm, feature(asm))]
+#![cfg_attr(all(target_os = "macos", usdt_need_asm_sym), feature(asm_sym))]
 
 use diesel::backend::Backend;
 use diesel::connection::{
